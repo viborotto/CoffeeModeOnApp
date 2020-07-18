e5104f0d3bd1ee5379a75d0c4dbb31d821249bbe
viborotto <vittoriaborotto@gmail.com>
2020-07-18 12:36:47 -0300

    Splash Layout

---
+++ app/src/main/java/br/com/vittoria/coffeemodeonapp/SplashActivity.kt
@@ -0,0 +1,11 @@
+package br.com.vittoria.coffeemodeonapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
