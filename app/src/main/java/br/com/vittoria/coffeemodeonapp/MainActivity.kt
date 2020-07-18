2e0ff043d2da816f71a3c8ffc0b668b4404d27ed
viborotto <vittoriaborotto@gmail.com>
2020-07-18 14:51:39 -0300

    Final Project

--- app/src/main/java/br/com/vittoria/coffeemodeonapp/MainActivity.kt
+++ app/src/main/java/br/com/vittoria/coffeemodeonapp/MainActivity.kt
@@ -2,10 +2,28 @@ package br.com.vittoria.coffeemodeonapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //Ao clicar em VAI fazer o calculo e disponibilizar no txv
+        btnVai.setOnClickListener {
+            Toast.makeText(this@MainActivity,"Sucesso!", Toast.LENGTH_LONG).show()
+
+            //Pegar texto do EditText para realizar o calculo
+            val quantidadeAgua = edtAgua.text.toString()
+
+            if (quantidadeAgua.isEmpty() || quantidadeAgua == "0"){
+                edtAgua.error = "Quantidade inválida"
+            } else {
+                //Verificacoes
+                val resultado = quantidadeAgua.toDouble() * 70 / 1000;
+                txvResultado.text = resultado.toString() + " gramas de café"
+            }
+        }
     }
 }
--- app/src/main/java/br/com/vittoria/coffeemodeonapp/SplashActivity.kt
+++ app/src/main/java/br/com/vittoria/coffeemodeonapp/SplashActivity.kt
@@ -1,11 +1,21 @@
 package br.com.vittoria.coffeemodeonapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        //Pausa a execução
+        Handler().postDelayed({
+            //chave dentro da funcao, funcao lambda
+            //inicia uma outra tela: login
+            startActivity(Intent(this@SplashActivity, MainActivity::class.java))
+            finish()
+        }, 5000)
     }
 }
